a15247fda31ba27cce1ee13f5762c25ccf6210f3
Alexander Basov <a.basov@synergetica.net>
2020-05-07 01:56:17 +0300

    Updates

---
+++ precache-manifest.52026d2b0da6e8abef937b0b6a9921a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ec37d77c1711c1a7616eef6290efa9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "473465cd44c7e239048b",
+    "url": "./static/css/2.b09f2111.chunk.css"
+  },
+  {
+    "revision": "ecf12d46988e08536ee0",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "473465cd44c7e239048b",
+    "url": "./static/js/2.37d360d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.37d360d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf12d46988e08536ee0",
+    "url": "./static/js/main.803a4cb1.chunk.js"
+  },
+  {
+    "revision": "bc9b0493d8858ff101ae",
+    "url": "./static/js/runtime-main.964bd8b7.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
